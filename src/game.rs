460f22142c7608a71de59de3aaa1ef2b19590be2
gak <gak@gak0.com>
2021-08-27 16:43:26 +1000

    fix door state toggle

--- src/game.rs
+++ src/game.rs
@@ -361,12 +361,12 @@ fn player_keyboard_action(
 fn warden_actions(
     mut commands: Commands,
     mut wardens: Query<(&Position, &Direction, &mut Action), With<Warden>>,
-    mut doors: Query<(Entity, &GridPosition, &mut Door, &mut Visible)>,
+    mut doors: Query<(Entity, &GridPosition, &mut Door)>,
     prisoners: Query<(Entity, &Position, &SpawnPoint), (With<Prisoner>, With<Escaping>)>,
 ) {
     for (warden_pos, warden_dir, mut action) in wardens.iter_mut() {
         let forward_pos = warden_pos.nearest_cell() + warden_dir;
-        for (door_ent, door_grid_pos, mut door, mut visible) in doors.iter_mut() {
+        for (door_ent, door_grid_pos, mut door) in doors.iter_mut() {
             if &forward_pos != door_grid_pos {
                 continue;
             }
@@ -374,10 +374,10 @@ fn warden_actions(
 
             match *door {
                 Door::Closed => {
-                    change_door_state(&mut commands, door_ent, false);
+                    change_door_state(&mut commands, door_ent, true);
                 }
                 Door::Open => {
-                    change_door_state(&mut commands, door_ent, true);
+                    change_door_state(&mut commands, door_ent, false);
                 }
             }
         }
@@ -413,18 +413,19 @@ fn change_door_state(commands: &mut Commands, door_ent: Entity, open: bool) {
     };
     let mut e = commands.entity(door_ent);
 
-    e.remove::<Walkable>()
-        .insert(NonWalkable)
+    e //
         .insert(door)
+        .remove::<Walkable>()
+        .remove::<NonWalkable>()
         .insert(Visible {
-            is_visible: open,
+            is_visible: !open,
             is_transparent: false,
         });
 
     if open {
-        e.remove::<Walkable>().insert(NonWalkable);
+        e.insert(Walkable);
     } else {
-        e.remove::<NonWalkable>().insert(Walkable);
+        e.insert(NonWalkable);
     }
 }
 
